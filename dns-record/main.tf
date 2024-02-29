2eea562fe745648f6d401b8c637968b3b8e216fb
Aset Madraimov <xiaset@gmail.com>
2024-02-29 14:38:18 +0600

    fix versions

--- dns-record/main.tf
+++ dns-record/main.tf
@@ -1,7 +1,7 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
-  required_version = "~> 1.5.0"
+  required_version = "~> 1.5"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
